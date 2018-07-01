2f6804a8dd176c98cc1737b32d44bbe7b46ec840
bpehlivan <bpehlivan235@gmail.com>
2018-07-01 14:08:33 +0300

    added custom token authantication

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CreateView, Logout, SetWeightRecord
+from .views import CreateView, Logout, SetWeightRecord, CustomAuthToken
 from rest_framework.authtoken import views
 
 urlpatterns = {
     path('getrecords/', CreateView.as_view()),
-    path('login/', views.obtain_auth_token),
+    path('login/', CustomAuthToken.as_view()),
     path('logout/', Logout.as_view()),
     path('record/', SetWeightRecord.as_view())
 }
--- api/views.py
+++ api/views.py
@@ -3,9 +3,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 import json
 
 c = connection.cursor()
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'username': user.username
+        })
 
 class Logout(APIView):
     permission_classes = (IsAuthenticated,)
